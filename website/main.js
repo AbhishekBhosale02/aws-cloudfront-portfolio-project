877de4d7b0ebdc82b0a47b72a06299e62ffa1955
Abhishek Bhosale <111589323+AbhishekBhosale02@users.noreply.github.com>
2025-11-17 22:30:59 +0530

    Add files via upload

---
+++ website/main.js
@@ -0,0 +1,33 @@
+//Anime for App Developer Text //////////////////////////////////////////////////////////////////////////
+var typed = new Typed(".text", {
+    strings: ["App Developer...", "Web Developer..."], 
+    typeSpeed: 100, 
+    backSpeed: 100, 
+    backDelay: 1000, 
+    loop: true,
+    showCursor: false // Add this line to hide the cursor
+
+});
+
+var typed = new Typed(".text2", {
+    strings: ["..."],
+    typeSpeed: 150,
+    backSpeed: 150,
+    backDelay: 1000,
+    loop: true,
+    showCursor: false // Add this line to hide the cursor
+});
+
+//Animation for Achivments "..."
+var typed = new Typed(".text3", {
+    strings: ["..."],
+    typeSpeed: 150,
+    backSpeed: 150,
+    backDelay: 1000,
+    loop: true,
+    showCursor: false // Add this line to hide the cursor
+});
+
+
+
+
